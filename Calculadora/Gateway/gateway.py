040719687aea84850105c748b2da22fb2e4a0a3f
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2019-09-16 17:53:13 -0500

    Update gateway.py

--- Calculadora/Gateway/gateway.py
+++ Calculadora/Gateway/gateway.py
@@ -4,36 +4,18 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/gateway', methods=['POST', 'GET'])
-def gateway():
-	if request.form['action'] == '+':
-	    
-	    	s1 = request.form["s1"]
-		s2 = request.form["s2"]
+@app.route("/<s1>/<s2>/<operacion>")
+def gateway(s1,s2,operacion):
+	if operacion == '+':
 		resultado=requests.get('http://suma:5000/'+s1+'/'+s2).text
-		return resultado
-	elif request.form['action'] == '-':
-                s1 = request.form["s1"]
-		s2 = request.form["s2"]
-		resultado=request.get('http://resta:5001'+s1+'/'+s2).text
-		return resultado
-	   
-	elif request.form['action'] == '*':
-                s1 = request.form["s1"]
-		s2 = request.form["s2"]
-		resultado=request.get('http://multiplicacion:5002'+s1+'/'+s2).text
-		return resultado
-	    
-	elif request.form['action'] == '/':
-                s1 = request.form["s1"]
-		s2 = request.form["s2"]
-	    	resultado=request.get('http://division:5003'+s1+'/'+s2).text
-		return resultado
+	elif operacion == '-':
+		resultado=request.get('http://resta:5000'+s1+'/'+s2).text	   
+	elif operacion == '*':
+		resultado=request.get('http://multiplicacion:5000'+s1+'/'+s2).text
+	elif operacion == '/':
+	    	resultado=request.get('http://division:5000'+s1+'/'+s2).text
+	return resultado
 
-
-@app.route('/cliente')
-def show_all():
-    	return render_template('cliente.html')
 
 if __name__ == "__main__":
     	app.run(host="0.0.0.0", debug=True)
