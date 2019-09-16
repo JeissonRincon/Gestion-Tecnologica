9d5cf3fc2d08ed182ce093034c2c7f7338d22894
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2019-09-16 18:03:32 -0500

    generar docker y requeriments

--- Calculadora/Cliente/cliente.py
+++ Calculadora/Cliente/cliente.py
@@ -6,4 +6,11 @@ app = Flask(__name__)
 
 @app.route('/cliente', methods=['POST','GET'])
 def cliente():
-    
+    s1 = request.form["s1"]
+    s2 = request.form["s2"]
+    operacion = request.form['action']
+    resultado = requests.get('http://gateway:5000'+s1+'/'+s2+'/'+operacion).text
+    return render_template('/cliente.html',string_variable=resultado)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=true)
