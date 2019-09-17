11df9028d42e613b26cfa664297db618d5890c06
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2019-09-16 22:21:29 -0500

    Update gateway.py

--- Calculadora/Gateway/gateway.py
+++ Calculadora/Gateway/gateway.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def gateway(s1,s2,operacion):
     
     if operacion == '+':
-	resultado=requests.get('http://suma:4000/'+s1+'/'+s2).text
+	resultado=requests.get('http://suma:5000/'+s1+'/'+s2).text
     elif operacion == '-':
-	resultado=request.get('http://resta:3000'+s1+'/'+s2).text	   
+	resultado=request.get('http://resta:5000'+s1+'/'+s2).text	   
     elif operacion == '*':
-	resultado=request.get('http://multiplicacion:2000'+s1+'/'+s2).text
+	resultado=request.get('http://multiplicacion:5000'+s1+'/'+s2).text
     elif operacion == '/':
-        resultado=request.get('http://division:1000'+s1+'/'+s2).text
+        resultado=request.get('http://division:5000'+s1+'/'+s2).text
     return resultado
 
 
