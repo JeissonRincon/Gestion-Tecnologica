6757892483f5ae68707d336a32c101f7912085aa
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2019-09-17 21:31:50 -0500

    Update cliente.py

--- Calculadora/Cliente/cliente.py
+++ Calculadora/Cliente/cliente.py
@@ -1,20 +1,22 @@
 from flask import Flask
-from flask import render_template,request, make_response
+from flask import render_template,request,make_response
 import requests
 
 app = Flask(__name__)
 
-@app.route('/calculadora')
-def calculadora():
+
+@app.route('/calcula')
+def calcula():
     	return render_template('calcula.html')
 
-@app.route('/cliente', methods=['POST','GET'])
-def cliente():
-    s1 = request.form["s1"]
-    s2 = request.form["s2"]
-    operacion = request.form['action']
-    resultado = requests.get('http://gateway:5000/'+s1+'/'+s2+'/'+operacion).text
-    return render_template('/calcula.html',string_variable=resultado)
+
+@app.route('/send', methods=['POST', 'GET'])
+def send():
+	operacion = request.form['action']	
+	s1 = request.form["s1"]
+	s2 = request.form["s2"]
+	resultado=requests.get('http://gateway:5000/'+s1+'/'+s2+'/'+operacion).text
+	return render_template('/calcula.html', string_variable=resultado)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    	app.run(host="0.0.0.0", debug=True)
