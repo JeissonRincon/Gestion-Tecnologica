e0e58eaf7ec1a64d0df9d48c45767198e6c04cc8
JeissonRincon <32280112+JeissonRincon@users.noreply.github.com>
2019-09-18 08:29:37 -0500

    Update gateway.py

--- Calculadora/Gateway/gateway.py
+++ Calculadora/Gateway/gateway.py
@@ -4,25 +4,18 @@ import requests
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def inicio():
-    return('<p>Hola, Mundo.</p>')
-
 @app.route("/<s1>/<s2>/<operacion>")
 def default(s1,s2,operacion):
-    
-    if operacion == '+':
-	resultado=requests.get('http://suma:5000/'+s1+'/'+s2).text
-    elif operacion == '-':
-	resultado=request.get('http://resta:5000/'+s1+'/'+s2).text	   
-    elif operacion == '*':
-	resultado=request.get('http://multiplicacion:5000/'+s1+'/'+s2).text
-    elif operacion == '/':
-        resultado=request.get('http://division:5000/'+s1+'/'+s2).text
-        
-    return resultado
-
+	if operacion == '+':
+		resultado=requests.get('http://suma:5000/'+s1+'/'+s2).text
+	elif operacion == '-':
+		resultado=requests.get('http://resta:5000/'+s1+'/'+s2).text
+	elif operacion == '*':
+		resultado=requests.get('http://multiplicacion:5000/'+s1+'/'+s2).text
+	elif operacion == '/':
+		resultado=requests.get('http://division:5000/'+s1+'/'+s2).text
+	return resultado
 
 if __name__ == "__main__":
     	app.run(host="0.0.0.0", debug=True)
+
